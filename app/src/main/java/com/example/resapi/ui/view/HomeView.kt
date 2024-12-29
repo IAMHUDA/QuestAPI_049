8f321b61c644ba0f17285a12e68b643f1aa58084
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-29 13:26:43 +0700

    membuat bagian home view dengan menambahkan destinasi navigasi

---
+++ app/src/main/java/com/example/resapi/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.resapi.ui.view
+
+import com.example.resapi.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
