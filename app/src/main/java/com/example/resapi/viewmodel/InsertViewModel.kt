c66fdf46c63592c36c9eb31635626a642eb3e18c
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-29 12:40:21 +0700

    add  fun insert mahasiswa

--- app/src/main/java/com/example/resapi/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/resapi/viewmodel/InsertViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.resapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.resapi.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(
     private val mhs: MahasiswaRepository
-) {
+): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = InsertUiEvent())
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
