a44af868539e59d7f46d3266e062f3faf4c24fc1
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-29 13:19:23 +0700

    membuat tampilan insert dengan memanggil destinasi navigasi

---
+++ app/src/main/java/com/example/resapi/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.resapi.ui.view
+
+import com.example.resapi.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
