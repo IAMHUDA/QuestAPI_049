c58d0aca989568cb04c4e3201dbe8a0478bd26cc
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-29 12:41:44 +0700

    add data class insertuievent

--- app/src/main/java/com/example/resapi/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/resapi/viewmodel/InsertViewModel.kt
@@ -2,8 +2,10 @@ package com.example.resapi.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.resapi.model.Mahasiswa
 import com.example.resapi.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
@@ -27,3 +29,38 @@ class InsertViewModel(
         }
     }
 }
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
